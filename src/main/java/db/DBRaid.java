6c25deb05591b2e8cf6e7ae54b5ee48220791663
Stephan Constantial <Sconstantial@yahoo.com>
2018-07-04 14:26:42 +0100

    end point

---
+++ src/main/java/db/DBRaid.java
@@ -0,0 +1,34 @@
+package db;
+
+import models.Pirate;
+import models.Raid;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
+public class DBRaid {
+
+    private static Session session;
+
+    public static List<Pirate> getRaidPirates(Raid raid) {
+        session = HibernateUtil.getSessionFactory().openSession();
+
+        List<Pirate> results = null;
+        try {
+            Criteria cr = session.createCriteria(Pirate.class);
+
+            cr.createAlias("raids","raid");
+            cr.add(Restrictions.eq("raid.id", raid.getId()));
+
+            results = cr.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+}
